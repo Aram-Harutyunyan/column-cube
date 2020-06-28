0a80e12e3ccf9e3ab2c944ea0dd43ba65d2fb004
Aram Harutyunyan <you@example.com>
2020-06-28 17:03:59 +0400

    Finalgit add .!

--- src/components/AddToColumn.js
+++ src/components/AddToColumn.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddToColumn=(props)=>{
 if(props.position){
 return (
-          <p ><h5 onClick={props.change}>{props.position}</h5></p>
+          <div className="box" ><h5 onClick={props.change}>{props.position}</h5></div>
           )
 }else return null;
 }
